42aaa2ba6beac3c0c5bfb1fce8214169f885e3ee
AMIT KUMAR <46942263+ak78158@users.noreply.github.com>
2020-08-11 19:26:40 +0530

    ITW-2 Android Dev.

---
+++ Bear Adviser/app/src/main/java/com/example/it_lab/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.it_lab;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Spinner;
+//import android.widget.RadioButton;
+//import android.widget.RadioGroup;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
+    }
+}
